dfa7f351947707fbd3f3e55367242306aa9f36ea
Robin David <66738389+robindavidgames@users.noreply.github.com>
2023-07-29 14:54:36 +0100

    Same bug, refixed again!

--- script.js
+++ script.js
@@ -29,8 +29,7 @@ function getActualTime(timeLeft) {
   const now = new Date(startTime.getTime() + (timeLeft * 1000));
   const hours = now.getHours();
   const minutes = now.getMinutes();
-  const seconds = now.getSeconds();
-  return `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+  return `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}`;
 }
 
 // Update countdown lines with formatted times
